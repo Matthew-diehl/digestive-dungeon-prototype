9da57f91e871b95d286844c989c351269fbc1f9f
Matthew-diehl <96633668+Matthew-diehl@users.noreply.github.com>
2023-11-29 23:40:15 -0500

    Update pxt.json, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -557,6 +557,25 @@ c 7 6 6 c 7 6 c c c c 6 c c c c
 6 7 7 6 c 6 c c 6 6 c 6 c 6 c c 
 6 7 7 6 c c c c 6 6 c c c 6 c c 
 `;
+            case "image19":
+            case "up arrow":return img`
+. . . . . . f f f f f f . . . . 
+. . . . f f e e e e f 2 f . . . 
+. . . f f e e e e f 2 2 2 f . . 
+. . . f e e e f f e e e e f . . 
+. . . f f f f e e 2 2 2 2 e f . 
+. . . f e 2 2 2 f f f f e 2 f . 
+. . f f f f f f f e e e f f f . 
+. . f f e 4 4 e b f 4 4 e e f . 
+. . f e e 4 d 4 1 f d d e f . . 
+. . . f e e e 4 d d d d f . . . 
+. . . . f f e e 4 4 4 e f . . . 
+. . . . . 4 d d e 2 2 2 f . . . 
+. . . . . e d d e 2 2 2 f . . . 
+. . . . . f e e f 4 5 5 f . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . f f f . . . . . . 
+`;
         }
         return null;
     })
